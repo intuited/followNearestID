e6d1833e111ad1f60eeedd65b4c7dd34ffd1d6e1
Ted Tibbetts <intuited@gmail.com>
2010-07-26 02:13:02 -0300

    rework with parametrized search depth; cleanup.

--- followNearestID.js
+++ followNearestID.js
@@ -1,34 +1,43 @@
-function getDescendantID(node) {
-  var child = node.firstChild;
-  while (child) {
-    if (child.id) return child.id;
-    var descendant_id = getDescendantID(child);
-    if (descendant_id) return descendant_id;
-    child = child.nextSibling;
-  };
-  return false;
-};
+function getPreviousID(node, maxdepth) {
+  /* Iterate backwards and upwards through the DOM from `node`.
+   * Previous siblings will be recursed to a depth of `maxdepth`,
+   *   or completely for `maxdepth < 0`.
+   */
 
-function getContextID(node) {
+  function getDescendantID(node, depth) {
+    if (depth == maxdepth) return false;
+    var child = node.firstChild;
+    while (child) {
+      if (child.id) return child.id;
+      var descendant_id = getDescendantID(child, depth + 1);
+      if (descendant_id) return descendant_id;
+      child = child.nextSibling;
+    };
+    return false;
+  };
+
   var previous = null;
   var id;
   while (node) {
     if (node.id) return node.id;
     /* Only recurse into siblings; children have already been searched. */
-    if (previous && (id = getDescendantID(node))) return id;
+    if (previous && (id = getDescendantID(node, 0))) return id;
     node = (previous = node.previousSibling) || node.parentNode;
   };
   return false;
 };
 
 function getSelectionNode() { return window.getSelection().baseNode; };
+
 function makeURI(base, id) {
   var hashIndex = base.lastIndexOf('#');
   if (hashIndex > -1) base = base.slice(0, hashIndex);
   return base + '#' + id;
 };
-function followNearestID() {
-  var id = getContextID(getSelectionNode());
+
+function followPreviousID(maxdepth) {
+  var id = getPreviousID(getSelectionNode(), maxdepth);
   if (id) { document.location.href = makeURI(document.location.href, id); };
 };
-followNearestID();
+
+followPreviousID(2);
