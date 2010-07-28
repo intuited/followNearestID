560a62b401329072ff2e77e2ece769dfe2e9d4bd
Ted Tibbetts <intuited@gmail.com>
2010-07-28 01:11:50 -0300

    revised for jslint compliance.

--- followPreviousID.js
+++ followPreviousID.js
@@ -1,54 +1,74 @@
-function getPreviousID(node, maxdepth) {
-  /* Iterate backwards and upwards through the DOM from `node`.
-   * Previous siblings will be recursed to a depth of `maxdepth`,
-   *   or completely for `maxdepth < 0`.
-   */
+/* Enables all of "The Good Parts"
+ *   except for "use strict" and Strict white space.
+ * I turned off Strict white space
+ *   mostly because it doesn't allow one-line if statements.
+ */
 
-  if (maxdepth == undefined) maxdepth = -1;
+/*jslint browser: true, onevar: true, undef: true, nomen: true, eqeqeq: true, plusplus: true, bitwise: true, regexp: true, newcap: true, immed: true */
+/*global window */
 
-  function getDescendantID(node, depth) {
-    if (depth == maxdepth) return false;
-    var child = node.firstChild;
-    while (child) {
-      if (child.id) return child.id;
-      var descendant_id = getDescendantID(child, depth + 1);
-      if (descendant_id) return descendant_id;
-      child = child.nextSibling;
+(function() {
+    var getPreviousID, getSelectionNode, makeURI, followPreviousID;
+
+    getPreviousID = function(node, maxdepth) {
+        /* Iterate backwards and upwards through the DOM from `node`.
+          * Previous siblings will be recursed to a depth of `maxdepth`,
+          *   or completely for `maxdepth < 0`.
+          */
+
+        if (maxdepth === undefined) { maxdepth = -1; }
+
+        var getDescendantID, previous, id;
+
+        getDescendantID = function(node, depth) {
+            var child, descendant_id;
+
+            if (depth === maxdepth) { return false; }
+            child = node.firstChild;
+            while (child) {
+                if (child.id) { return child.id; }
+                descendant_id = getDescendantID(child, depth + 1);
+                if (descendant_id) { return descendant_id; }
+                child = child.nextSibling;
+            }
+            return false;
+        };
+
+        previous = null;
+        while (node) {
+            if (node.id) { return node.id; }
+            /* Only recurse into siblings; children have already been searched. */
+            if (previous && (id = getDescendantID(node, 0))) { return id; }
+            node = (previous = node.previousSibling) || node.parentNode;
+        }
+        return false;
     };
-    return false;
-  };
 
-  var previous = null;
-  var id;
-  while (node) {
-    if (node.id) return node.id;
-    /* Only recurse into siblings; children have already been searched. */
-    if (previous && (id = getDescendantID(node, 0))) return id;
-    node = (previous = node.previousSibling) || node.parentNode;
-  };
-  return false;
-};
+    getSelectionNode = function() { return window.getSelection().baseNode; };
 
-function getSelectionNode() { return window.getSelection().baseNode; };
+    makeURI = function(base, id) {
+        var hashIndex;
+        hashIndex = base.lastIndexOf('#');
+        if (hashIndex > -1) { base = base.slice(0, hashIndex); }
+        return base + '#' + id;
+    };
 
-function makeURI(base, id) {
-  var hashIndex = base.lastIndexOf('#');
-  if (hashIndex > -1) base = base.slice(0, hashIndex);
-  return base + '#' + id;
-};
+    followPreviousID = function(maxdepth) {
+        /* Iterate backward and upward through the DOM,
+          *   looking for a node with an ID attribute.
+          * `maxdepth` determines the depth to which previous nodes are searched.
+          * By default this will be a depth of 2.
+          * Passing 0 will prevent searching in subnodes.
+          * Passing -1 will result in a full traversal of the previous DOM nodes.
+          */
+        if (maxdepth === undefined) { maxdepth = 2; }
 
-function followPreviousID(maxdepth) {
-  /* Iterate backward and upward through the DOM,
-   *   looking for a node with an ID attribute.
-   * `maxdepth` determines the depth to which previous nodes are searched.
-   * By default this will be a depth of 2.
-   * Passing 0 will prevent searching in subnodes.
-   * Passing -1 will result in a full traversal of the previous DOM nodes.
-   */
-  if (maxdepth == undefined) maxdepth = 2;
+        var id;
+        id = getPreviousID(getSelectionNode(), maxdepth);
+        if (id) {
+            document.location.href = makeURI(document.location.href, id);
+        }
+    };
 
-  var id = getPreviousID(getSelectionNode(), maxdepth);
-  if (id) { document.location.href = makeURI(document.location.href, id); };
-};
-
-followPreviousID();
+    followPreviousID();
+}());
